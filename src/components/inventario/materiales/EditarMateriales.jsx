0d8f2a5772616c917382804bb154e7b27b52713c
bethsbm <maritza.sosa@unah.hn>
2022-11-19 14:12:34 -0600

    Corrección

--- src/components/inventario/materiales/EditarMateriales.jsx
+++ src/components/inventario/materiales/EditarMateriales.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useNavigate } from "react-router-dom";
